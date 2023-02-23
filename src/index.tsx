ff7584dc7586d212aab2c41413cae9f17779d22c
Asadul islam <islamasadul327@gmail.com>
2023-02-23 20:26:09 +0600

    updated

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "./page.module.css";
+import Card from "./app/components/card";
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <Card />
+    </main>
+  );
+}
